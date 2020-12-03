cc744d55f5deeaa367e3a5cdbce684b1be24c01d
Odette Bahome <odettebahome@gmail.com>
2020-12-03 11:14:22 +0000

    spinner

--- spinner2.js
+++ spinner2.js
@@ -1,8 +1,7 @@
-//process.stdout.write('hello from spinner1.js... \rheyyy\n');
 const spinner = function(arr) {
   let newArr = arr.concat(arr);
   const len = newArr.length;
-  //console.log(newArr);
+  //console.log(len);
   let timer = 100;
   for (let i = 0; i < len; i++) { 
     setTimeout(() => {process.stdout.write(`\r'${newArr[i]}`);}, timer);
